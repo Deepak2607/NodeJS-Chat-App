f5b8a08b230610fbdbc8d93e7a0d8b11c47c57c4
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-10-06 03:10:20 +0530

    ''

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path= require('path');
 const express= require('express');
+const socketIO= require('socket.io');
+const http= require('http');
 
 const app= express();
 
-const port= process.env.PORT || 3000;
+const port= process.env.PORT || 8000;
 const publicPath= path.join(__dirname, '../public');
+
+const server= http.createServer(app);
+const io= socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=> {
+    console.log('New user connected');
+    
+    socket.on('disconnect',()=> {
+        console.log('User disconnected');
+    })
+})
 
-app.listen(port, ()=> {
+
+
+
+server.listen(port, ()=> {
     console.log(`local server is started at ${port}`);
 })
