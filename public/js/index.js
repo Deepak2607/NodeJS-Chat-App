cf651937129f933da12a5e22a6b3f03ffb9bb619
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-10-06 19:54:18 +0530

    ''

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+const socket= io();
+
+socket.on('connect',()=> {
+    console.log('Connected to server');
+    })
+                                             // when server is connected..it notifies client that it    is connected to server.(inside console)
+socket.on('disconnect',()=> {
+    console.log('Disconnected to server');
+})
+                                             // when server is disconnected..it notifies client that    it is disconnected to server.(inside console)
+//socket.on('newEmail',(email)=> {
+//    console.log('newEmail', email);
+//})
+//
+//socket.emit('createEmail',{
+//        to:'deepak@gmail.com',
+//        text:'Hello'
+//    })
+
+socket.on('newMessage',(message)=> {
+    console.log('newMessage', message);
+})
+
+socket.emit('createMessage',{
+        to:'deepak@gmail.com',
+        text:'Hello'
+    })
+
+
--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const express= require('express');
 const socketIO= require('socket.io');
 const http= require('http');
 
-const app= express();
-
+const app= express();                            
+                                                //server side..(on cmd)
 const port= process.env.PORT || 8000;
 const publicPath= path.join(__dirname, '../public');
 
@@ -15,14 +15,35 @@ app.use(express.static(publicPath));
 
 io.on('connection',(socket)=> {
     console.log('New user connected');
-    
+                                                // when client is connected..it notifies server that new 
+                                                // user is connected.(inside cmd)
     socket.on('disconnect',()=> {
         console.log('User disconnected');
     })
+                                                // when client is disconnected..it notifies server that
+                                                // user is disconnected.(inside cmd)                     
+//    socket.emit('newEmail',{
+//        from:'admin@gmail.com',
+//        text:'Welcome to chat app',
+//        createdAt:123
+//    })
+//    
+//    socket.on('createEmail',(email)=> {
+//        console.log('createEmail', email)
+//    })
+    
+    socket.emit('newMessage',{
+        from:'admin@gmail.com',
+        text:'Welcome to chat app',
+        createdAt:123
+    })
+    
+    socket.on('createMessage',(message)=> {
+        console.log('createMessage', message)
+    })
 })
 
-
-
+                                                
 
 server.listen(port, ()=> {
     console.log(`local server is started at ${port}`);
