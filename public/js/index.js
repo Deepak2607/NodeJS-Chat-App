dda82380e7850c54b711715c640b47ba33166b15
Deepak Kumrawat <deepakkumrawat8@gmail.com>
2018-10-24 18:26:43 +0530

    ''

--- public/js/index.js
+++ public/js/index.js
@@ -15,6 +15,14 @@ socket.on('newMessage',(message)=> {
     jQuery('.messages').append(li);
 })
 
+socket.on('newLocationMessage',(message)=>{
+    
+    const li= jQuery('<li></li>').text(`${message.from}: `);
+    const a= jQuery('<a target="_blank">My Location</a>').attr('href', message.url);
+    li.append(a);
+    jQuery('.messages').append(li);
+})
+
 jQuery('form').submit((e)=> {
 //    alert('submitted');
     e.preventDefault();
@@ -25,6 +33,23 @@ jQuery('form').submit((e)=> {
     })
     jQuery('.message').val(null);
  
+})
+
+jQuery('.send-location').on('click',function(){
+    if(!navigator.geolocation){
+        return alert('Geolocation not supproted by your browser');
+    }
+    
+    navigator.geolocation.getCurrentPosition(function(position){
+        console.log(position);
+        
+        socket.emit('createLocationMessage',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        })
+    },function(){
+        alert('Unable to fetch location');
+    },{timeout: 30000, enableHighAccuracy: true, maximumAge: 75000})
 })
 
 
--- server/server.js
+++ server/server.js
@@ -16,12 +16,7 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=> {
     console.log('New user connected');
                                                 // when client is connected..it notifies server that new 
-                                                // user is connected.(inside cmd)
-    socket.on('disconnect',()=> {
-        console.log('User disconnected');
-    })
-                                                // when client is disconnected..it notifies server that
-                                                // user is disconnected.(inside cmd)                    
+                                                // user is connected.(inside cmd)                 
     
     socket.emit('newMessage',{
         from:'Admin',
@@ -43,7 +38,7 @@ io.on('connection',(socket)=> {
             text:message.text,
             createdAt:new Date().getTime()
         })
-        
+            
 //        socket.broadcast.emit('newMessage',{
 //            from:message.from,
 //            text:message.text,
@@ -51,6 +46,21 @@ io.on('connection',(socket)=> {
 //        })
         
     })
+    
+     socket.on('createLocationMessage',(message)=>{
+        
+        io.emit('newLocationMessage',{
+            from:'Admin',
+            url:`https://www.google.com/maps?q=${message.latitude},${message.longitude}`,
+            createdAt:new Date().getTime()
+        })
+    })
+    
+        socket.on('disconnect',()=> {
+        console.log('User disconnected');
+    })
+                                                // when client is disconnected..it notifies server that
+                                                // user is disconnected.(inside cmd)   
     
 //    socket.emit('newEmail',{
 //        from:'admin@gmail.com',
